c2de244e3a2ddb31341fa8174fc2b3810978ecc2
qianyu12138 <1464697820@qq.com>
2019-08-16 19:08:02 +0800

    更新car

--- src/main/java/cn/yd/carrentalsystem/controller/UserController.java
+++ src/main/java/cn/yd/carrentalsystem/controller/UserController.java
@@ -19,44 +19,40 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping("/regist")
-    public  String Regist(String phone,String password)
-    {
+    public String Regist(String phone, String password) {
         boolean isExist = userService.getUserExist(phone);
-        if(!isExist&& !StringUtils.isEmpty(phone) &&!StringUtils.isEmpty(password)){
-            userService.regist(phone,password);
+        if (!isExist && !StringUtils.isEmpty(phone) && !StringUtils.isEmpty(password)) {
+            userService.regist(phone, password);
         }
         return "{\"status\":1}";
     }
+
     @RequestMapping("toRegist")
-    public String toRegist(){
+    public String toRegist() {
         return "regist";
     }
+
     @RequestMapping("/user/toLogin")
-    public  String toLogin()
-    {
+    public String toLogin() {
         return "html/user/login";
     }
 
     @RequestMapping("/user/login")
- public  String login(HttpServletRequest request, String phone, String password)
-     {
-        User user= userService.login(phone,password);
-        if (user==null)
-        {
-            request.setAttribute("er","账号或密码错误");
+    public String login(HttpServletRequest request, String phone, String password) {
+        User user = userService.login(phone, password);
+        if (user == null) {
+            request.setAttribute("er", "账号或密码错误");
             return "login";
         }
-       request.getSession().setAttribute("user",user);
-     return  "index";
-     }
-
-
+        request.getSession().setAttribute("user", user);
+        return "index";
+    }
 
     @ResponseBody
     @RequestMapping("/user/getUserExist/{name}")
-    public String getUserExist(@PathVariable("name") String username){
+    public String getUserExist(@PathVariable("name") String username) {
         boolean isExist = userService.getUserExist(username);
 
-        return "{\"isExist\":"+isExist+"}";
+        return "{\"isExist\":" + isExist + "}";
     }
 }
